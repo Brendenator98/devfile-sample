c17553c04118fdcc0903c2c3fc34a3905baa5688
null <null>
2023-07-25 19:05:44 +0000

    added stuff

--- server.js
+++ server.js
@@ -2,24 +2,7 @@ const Prometheus = require('prom-client')
 const express = require('express');
 const http = require('http');
 
-import { Client } from "pg";
 
-const serviceHost = process.env.MY_DATABASE_SERVICE_HOST;
-  const user = process.env.DB_USERNAME;
-  const password = process.env.DB_PASSWORD;
-  const databaseName = process.env.POSTGRESQL_DATABASE
-  const connectionString =
-    `postgresql://${user}:${password}@${serviceHost}:5432/${databaseName}`;
-
-const c = new Client(connectionString)
-
-try {
-    c.connect()
-    console.log('conneted ')
-
-} catch (err) {
-    console.log(err)
-}
 
 
 Prometheus.collectDefaultMetrics();
