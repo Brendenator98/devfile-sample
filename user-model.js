9c0df69853da9f5019a26db250bc23f6633d3f46
null <null>
2023-07-25 18:57:00 +0000

    added stuff

--- user-model.js
+++ user-model.js
@@ -1,9 +1,18 @@
+import { Client } from "pg";
+
 const serviceHost = process.env.MY_DATABASE_SERVICE_HOST;
   const user = process.env.DB_USERNAME;
   const password = process.env.DB_PASSWORD;
   const databaseName = process.env.POSTGRESQL_DATABASE
   const connectionString =
     `postgresql://${user}:${password}@${serviceHost}:5432/${databaseName}`;
-  connectionOptions = { connectionString };
 
-  const pool = new Pool(connectionOptions);
+const c = new Client(connectionString)
+
+try {
+    c.connect()
+    console.log('conneted ')
+
+} catch (err) {
+    console.log(err)
+}
